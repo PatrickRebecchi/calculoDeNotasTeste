7c037446600cacf61319a48d6660d86af147f4c1
Patrick <patrickrebecchi@hotmail.com>
2023-07-13 13:41:37 -0300

    Adicionando o limitador de notas de 0 a 10

--- src/Notas.java
+++ src/Notas.java
@@ -7,15 +7,29 @@ public class Notas {
     double historia;
     double educacaoFisica;
 
+    boolean validarNotas() {
+        if (portugues < 0 || portugues > 10 ||
+                matematica < 0 || matematica > 10 ||
+                arte < 0 || arte > 10 ||
+                ciencia < 0 || ciencia > 10 ||
+                historia < 0 || historia > 10 ||
+                educacaoFisica < 0 || educacaoFisica > 10) {
+            return false;
+        }
+        return true;
+    }
 
-    void NotaFinal(){
-        double notaFinal;
-        notaFinal = (portugues + matematica + arte + ciencia * historia + educacaoFisica) / 6;
+    void NotaFinal() {
+        if (!validarNotas()) {
+            System.out.println("As notas devem estar no intervalo de 0 a 10.");
+            return;
+        }
+        double notaFinal = (portugues + matematica + arte + ciencia + historia + educacaoFisica) / 6;
         System.out.printf("Nota final: %.1f%n", notaFinal);
-        if (notaFinal >= 6){
-            System.out.println("Parabens você passou");
+        if (notaFinal >= 6) {
+            System.out.println("Parabéns, você passou!");
         } else {
-            System.out.println("Infelizmente você repetiu.. Até ano que vem!!!");
+            System.out.println("Infelizmente você repetiu... Até o próximo ano!!!");
         }
     }
 }

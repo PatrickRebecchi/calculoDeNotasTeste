cc59c97b3cd1beeaa18cee42ee33a6278a9b099b
Patrick <patrickrebecchi@hotmail.com>
2023-07-13 13:30:14 -0300

    Revitalizando o código, melhorias nas classes alunos e principal

--- src/Aluno.java
+++ src/Aluno.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 public class Aluno {
     String nome;
     int idade;
     int numeroAluno;
     String anoEscolar;
+
+    public void lerInformacoes() {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite o seu Nome: ");
+        nome = leitura.nextLine();
+        System.out.println("Digite a sua idade: ");
+        idade = leitura.nextInt();
+        System.out.println("Digite o número do aluno na chamada: ");
+        numeroAluno = leitura.nextInt();
+        leitura.nextLine();
+        System.out.println("Digite o ano que o aluno está cursando: ");
+        anoEscolar = leitura.nextLine();
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -7,23 +7,16 @@ public class Principal {
         Aluno aluno1 = new Aluno();
         Notas nota01 = new Notas();
         Locale.setDefault(Locale.US);
+        
         System.out.println("***************************************");
-        System.out.println("Seja bem-vindo ao medido de notas");
+        System.out.println("Seja bem-vindo ao medidor de notas");
         System.out.println("***************************************");
-        System.out.println("Digite o seu Nome: ");
-        aluno1.nome = leitura.nextLine();
-        System.out.println("Digite a sua idade; ");
-        aluno1.idade = leitura.nextInt();
-        System.out.println("Digite o numero do aluno na chamada: ");
-        aluno1.numeroAluno = leitura.nextInt();
-        System.out.println("Digite o ano que o aluno está cursando: ");
-        aluno1.anoEscolar = leitura.nextLine();
-        leitura.nextLine();
-        System.out.printf("Obrigado pelas informções %s%n", aluno1.nome);
-        System.out.println("""
-                Aluno localizado
-                Liberado para conferir as notas
-                """);
+
+        aluno1.lerInformacoes(); // Chamada do método para ler as informações do aluno
+
+        System.out.printf("Obrigado pelas informações, %s%n", aluno1.nome);
+        System.out.println("Aluno localizado");
+        System.out.println("Liberado para conferir as notas");
         System.out.println("Digite a nota de Portugues");
         nota01.portugues = leitura.nextInt();
         System.out.println("Digite a nota de Matematica");

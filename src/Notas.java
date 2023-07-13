5933b479c4422cd358a37e9dcc5d6c03c1e14eb1
Patrick <patrickrebecchi@hotmail.com>
2023-07-13 14:36:27 -0300

    Johando a leitura das notas na classe Notas

--- src/Notas.java
+++ src/Notas.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Notas {
 // notas que vou usar na parte principal
     double portugues;
@@ -6,6 +8,38 @@ public class Notas {
     double ciencia;
     double historia;
     double educacaoFisica;
+
+    void lerNotas(Scanner leitura) {
+        do {
+            System.out.println("Digite a nota de Português (0 a 10):");
+            portugues = leitura.nextDouble();
+        } while (portugues < 0 || portugues > 10);
+
+        do {
+            System.out.println("Digite a nota de Matemática (0 a 10):");
+            matematica = leitura.nextDouble();
+        } while (matematica < 0 || matematica > 10);
+
+        do {
+            System.out.println("Digite a nota de Arte (0 a 10):");
+            arte = leitura.nextDouble();
+        } while (arte < 0 || arte > 10);
+
+        do {
+            System.out.println("Digite a nota de Ciência (0 a 10):");
+            ciencia = leitura.nextDouble();
+        } while (ciencia < 0 || ciencia > 10);
+
+        do {
+            System.out.println("Digite a nota de História (0 a 10):");
+            historia = leitura.nextDouble();
+        } while (historia < 0 || historia > 10);
+
+        do {
+            System.out.println("Digite a nota de Educação Física (0 a 10):");
+            educacaoFisica = leitura.nextDouble();
+        } while (educacaoFisica < 0 || educacaoFisica > 10);
+    }
 
     boolean validarNotas() {
         if (portugues < 0 || portugues > 10 ||
@@ -32,5 +66,6 @@ public class Notas {
             System.out.println("Infelizmente você repetiu... Até o próximo ano!!!");
         }
     }
+
 }
 
--- src/Principal.java
+++ src/Principal.java
@@ -18,35 +18,7 @@ public class Principal {
         System.out.println("Aluno localizado");
         System.out.println("Liberado para conferir as notas");
 
-        do {
-            System.out.println("Digite a nota de Português (0 a 10):");
-            nota01.portugues = leitura.nextDouble();
-        } while (nota01.portugues < 0 || nota01.portugues > 10);
-
-        do {
-            System.out.println("Digite a nota de Matemática (0 a 10):");
-            nota01.matematica = leitura.nextDouble();
-        } while (nota01.matematica < 0 || nota01.matematica > 10);
-
-        do {
-            System.out.println("Digite a nota de Arte (0 a 10):");
-            nota01.arte = leitura.nextDouble();
-        } while (nota01.arte < 0 || nota01.arte > 10);
-
-        do {
-            System.out.println("Digite a nota de Ciência (0 a 10):");
-            nota01.ciencia = leitura.nextDouble();
-        } while (nota01.ciencia < 0 || nota01.ciencia > 10);
-
-        do {
-            System.out.println("Digite a nota de História (0 a 10):");
-            nota01.historia = leitura.nextDouble();
-        } while (nota01.historia < 0 || nota01.historia > 10);
-
-        do {
-            System.out.println("Digite a nota de Educação Física (0 a 10):");
-            nota01.educacaoFisica = leitura.nextDouble();
-        } while (nota01.educacaoFisica < 0 || nota01.educacaoFisica > 10);
+        nota01.lerNotas(leitura);
 
         nota01.NotaFinal();
     }

7c44b3fe20568f98a1900b4f60ffc1e0720b174b
Patrick <patrickrebecchi@hotmail.com>
2023-07-13 03:34:23 -0300

    adc notas

---
+++ src/Notas.java
@@ -0,0 +1,22 @@
+public class Notas {
+
+    double portugues;
+    double matematica;
+    double arte;
+    double ciencia;
+    double historia;
+    double educacaoFisica;
+
+
+    void NotaFinal(){
+        double notaFinal;
+        notaFinal = (portugues + matematica + arte + ciencia * historia + educacaoFisica) / 6;
+        System.out.printf("Nota final: %.1f%n", notaFinal);
+        if (notaFinal >= 6){
+            System.out.println("Parabens você passou");
+        } else {
+            System.out.println("Infelizmente você repetiu.. Até ano que vem!!!");
+        }
+    }
+}
+
